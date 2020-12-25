ee49dc69231c237d4bd6194edc4253a4ba103de9
ranameenu <meenurana32@gmail.com>
2020-12-25 23:15:18 +0530

    functn pass to variable

--- filter.js
+++ filter.js
@@ -1,19 +1,12 @@
-function swap() {
-  var a, b, c;
-  a = document.getElementById("demo1").value;
-  b = document.getElementById("demo2").value;
-  c = a;
-  a = b;
-  b = c;
-  document.getElementById("demo3").value = a;
-  document.getElementById("demo4").value = b;
+function mypro() {
+  return console.log('hello world...');
 }
 
-const arr = [1, 5, 3, 8, 10, 45, 35, 34];
-var i = 0;
+let test = mypro;
+test();
 
-for (i = 0; i < arr.length; i++) {
-  if (arr[i] % 2 == 0) {
-    console.log(arr[i]);
-  }
-}
+// function greet(testfunc) {
+//   console.log(testfunc());
+// }
+
+// greet(mypro);
